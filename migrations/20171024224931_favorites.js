68751d66d5fa843959b3b852e8bdc883727bfba4
fmd123 <fmdavis6@gmail.com>
2017-10-25 00:06:07 -0600

    need part4 routes(4/6) and part5 all routes. favorites migrate & seed

---
+++ migrations/20171024224931_favorites.js
@@ -0,0 +1,15 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('favorites', (table)=>{
+  table.increments();
+  table.integer('book_id').notNullable().references('id').inTable('books').onDelete('CASCADE').onUpdate('CASCADE').index();
+  table.integer('user_id').notNullable().references('id').inTable('users').onDelete('CASCADE').onUpdate('CASCADE').index();
+  table.timestamps(true, true);
+
+})
+};
+
+exports.down = function(knex, Promise) {
+return knex.schema.dropTable('favorites')
+};
+//I changed favorites seed name so that it would run AFTER the other seeds on which it depended for foreign keys.
+//https://github.com/tgriesser/knex/issues/637
--- routes/token.js
+++ routes/token.js
@@ -6,13 +6,40 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../knex');
 const humps =require('humps');
+const boom = require('boom');
 
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt-as-promised');
+//what is promisifying bcr?
+// saltRounds is the work factor for bcrypt 2 to the tenth iterations
 const saltRounds = 10;
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
 
-router.get
 
+console.log("before everything");
+router.get('/token', (req, res,next) => {
+  // compare the req.body token value and the req.cookies (saved by cookie-parser)
+  if(Object.keys(req.body).length===0 && Object.keys(req.cookies).length===0){
+    //the status is fine but there's no cookie?
+    res.status(200)
+    res.send(false)
+  }else if (Object.keys(req.cookies).length>0){
+    res.status(200)
+    res.send(true);
+  }
+});
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+router.post('/token', (req, res, next)=>{
+  //check if email and password have been entered (this is test 3 & 4)
+  //use knex to find the user row associated with the email
+  //bcrypt.compare hashed password in request and the hashed password from user table
+  //create claim and token
+  //respond with cookie
+  //
+})//end of post token
+router.delete('/token', (req, res, next)=>{
+  res.clearCookie('token');
+  res.end();
+});
 module.exports = router;
